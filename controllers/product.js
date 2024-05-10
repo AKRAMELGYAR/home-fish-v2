d07484da124c1fb5836711a521388e5f875cb9fd
MostafaKolkela <115589891+MostafaKolkela@users.noreply.github.com>
2024-05-11 02:30:30 +0300

    10/5

--- controllers/product.js
+++ controllers/product.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 const appError = require('../util/AppErorr')
 
 const  postAddProduct = async (req, res, next) => {
-  try{
+  
     const { name, price, description,img_url,quantity } = req.body;
     const newProduct = new Product({
       name: name,
@@ -11,12 +11,12 @@ const  postAddProduct = async (req, res, next) => {
       img_url: img_url,
       quantity: quantity,
     })
-    await newProduct.save();
-    res.status(201).json({ status : "SUCCESS" ,Product : newProduct});
-  }catch{
-    const error = appError.create('Error adding product', 500, "fail")
-    return next(error)
-  }
+    await newProduct.save()
+    .then(()=>{
+      res.status(201).json({ status : "SUCCESS" ,Product : newProduct})
+      console.log(newProduct)
+    })
+    .catch((err)=>{console.log(err)}) 
 } 
 
 const  getProducts = async(req, res, next)=>{
